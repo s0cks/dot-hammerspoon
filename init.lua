ae5dcc1c6215cc2078a7c302d707cd4cc6dbbabb
tazz <tazz@tazz.cloud>
2025-12-26 08:00:32 -0500

    refactor: 🔨 code cleanup

--- init.lua
+++ init.lua
@@ -1,17 +1,28 @@
 require('hs.ipc')
 
-local hsconfig_dir = os.getenv('HOME') .. '/.config/hammerspoon'
-print('-- hammerspoon configuration dir: ' .. hsconfig_dir)
-local paths = {
-  hsconfig_dir .. '/?.lua',
-  hsconfig_dir .. '/lua/?.lua',
-  hsconfig_dir .. '/lua/?/init.lua',
-}
-package.path = package.path .. ';' .. table.concat(paths, ';')
-print('-- package.path: ' .. package.path)
+local function get_hammerspoon_home()
+  local dir = os.getenv('HAMMERSPOON_HOME')
+  if not dir then
+    dir = os.getenv('HOME') .. '/.config/hammerspoon'
+  end
+  return dir
+end
+
+local function add_to_rtp(dir)
+  local paths = {
+    dir .. '/?.lua',
+    dir .. '/lua/?.lua',
+    dir .. '/lua/?/init.lua',
+  }
+  package.path = package.path .. ';' .. table.concat(paths, ';')
+end
+
+local hs_home = get_hammerspoon_home()
+print('-- hammerspoon home: ' .. hs_home)
+add_to_rtp(hs_home)
+print('-- runtime path: ' .. package.path)
 
 --- hotkeys
-require('hotkeys')
 require('url-events')
 
 --- Configure & start the app watcher
